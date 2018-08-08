84d53ca67cf849d7e5389e108790cc3a162e1af4
Devin Soni <devin.soni@airbnb.com>
2018-08-08 12:26:45 -0700

    fix pypi setup and bump version

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup
 
 setup(
     name='Adversary',
-    version='1.0',
+    version='1.1',
     packages=['tests', 'Adversary'],
     url='https://github.com/airbnb/artificial-adversary',
     license='MIT',
     author='Devin Soni',
     author_email='devinsoni1010@gmail.com',
-    description='Creates adversarial text examples for machine learning models'
+    description='Creates adversarial text examples for machine learning models',
+    install_requires=[
+        'pandas',
+        'nltk',
+        'textblob'
+    ],
 )
