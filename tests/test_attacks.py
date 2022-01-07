1fd9b1249ef20f8b08f999923b0b516c85040068
Ezaldeen Sahb <30810111+Ezaldeen99@users.noreply.github.com>
2022-01-07 14:22:47 +0300

    Update test_attacks.py

--- tests/test_attacks.py
+++ tests/test_attacks.py
@@ -6,7 +6,3 @@ def test_num_to_word():
     assert (num_to_word('dog') == 'dog')
 
 
-print(emojis_attack("please send me 10,000 US DOLLARS to bank of scamland"))
-print(advanced_emojis_attack("please send me 10,000 US DOLLARS to bank of scamland"))
-print(surrounding_chars("please wire me 10,000 US DOLLARS to the bank of baghdad"))
-print(homophones_chars("please wire me 10,000 US DOLLARS to the bank of baghdad"))
